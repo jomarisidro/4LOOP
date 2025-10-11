9bce1954c66e5afe25d9377c0c7a9162c830a188
jomarisidro <jomar.isidro@my.jru.edu>
2025-10-11 15:03:37 +0800

    Resync all project files

--- src/app/officers/workbench/acceptedonlinerequest/page.jsx
+++ src/app/officers/workbench/acceptedonlinerequest/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = "force-dynamic";
 import AcceptedOnlineRequestForm from '../../../components/officers/AcceptedOnlineRequestForm';
 
 export default function AcceptedOnlineRequestPage() {
